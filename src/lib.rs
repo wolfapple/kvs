f0272a5ff8bd43cd9bbbe997c7952d79b4c1b43d
Jonghoon Kim <barney@toss.im>
2025-09-22 18:44:48 +0900

    Make the tests compile

---
+++ src/lib.rs
@@ -0,0 +1,19 @@
+pub struct KvStore {}
+
+impl KvStore {
+    pub fn new() -> KvStore {
+        todo!()
+    }
+
+    pub fn set(&mut self, key: String, value: String) {
+        todo!()
+    }
+
+    pub fn get(&mut self, key: String) -> Option<String> {
+        todo!()
+    }
+
+    pub fn remove(&mut self, key: String) {
+        todo!()
+    }
+}
