666472d24598be2418c0c0a9df624e2fa9507b7e
Barney Kim <9wolfapple@gmail.com>
2025-09-24 00:45:48 +0900

    Accept command line arguments

--- src/bin/kvs.rs
+++ src/bin/kvs.rs
@@ -1,3 +1,34 @@
+use std::process::exit;
+use clap::{Parser, Subcommand};
+
+#[derive(Debug, Parser)]
+#[clap(version)]
+struct Args {
+    #[command(subcommand)]
+    cmd: Commands,
+}
+
+#[derive(Subcommand, Debug)]
+enum Commands {
+    Set { key: String, value: String },
+    Get { key: String },
+    Rm { key: String },
+}
+
 fn main() {
-    println!("Hello, world!");
+    let args = Args::parse();
+    match args.cmd {
+        Commands::Set { key, value } => {
+            eprintln!("unimplemented");
+            exit(1);
+        }
+        Commands::Get { key } => {
+            eprintln!("unimplemented");
+            exit(1);
+        }
+        Commands::Rm { key } => {
+            eprintln!("unimplemented");
+            exit(1);
+        }
+    }
 }
